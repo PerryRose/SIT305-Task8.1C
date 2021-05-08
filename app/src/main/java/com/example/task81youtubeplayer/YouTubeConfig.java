27338b2752f1e46a326b07af5f13fd86091a04b9
PERRY ROSE <roseper@deakin.edu.au>
2021-05-08 14:34:29 +1000

    Init cimmit

--- app/src/main/java/com/example/task81youtubeplayer/YouTubeConfig.java
+++ app/src/main/java/com/example/task81youtubeplayer/YouTubeConfig.java
@@ -2,7 +2,7 @@ package com.example.task81youtubeplayer;
 
 public class YouTubeConfig {
 
-    public static final String API_KEY = "AIzaSyDGzv8Fzhf1lHkk1uFelWUeBMcJhukTaZI";
+    public static final String API_KEY = "REMOVED_FOR_GITHUB";
     public static final String URL_KEY = "url";
 
     public YouTubeConfig() {
